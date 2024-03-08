c062a3699fccb2258f98707569f3cc3448cdc47d
Samallll <samalchokli@gmail.com>
2024-03-08 17:51:04 +0530

    style(event) : display profit for expired events

--- frontend/src/components/event/EventCard.jsx
+++ frontend/src/components/event/EventCard.jsx
@@ -58,6 +58,13 @@ function EventCard({ eventDetails }) {
                   Seats Available: {eventDetails?.event.seatsAvailable}
                 </span>
               </li>
+              <li className="text-gray-400">
+                {eventDetails?.event?.profit && eventDetails?.eventStatus === "EXPIRED" ? (
+                  <span className={`text-${eventDetails?.event?.profit > 0 ? 'green' : 'red'}-600`}>
+                    Profit : {eventDetails?.event.profit} Rs
+                  </span>
+                ) : null}
+              </li>
             </ul>
           </div>
         </div>
