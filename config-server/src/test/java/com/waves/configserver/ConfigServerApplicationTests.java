d14a77f7e7cabf545f036b45602654c1d8412a00
Samallll <samalchokli@gmail.com>
2024-03-14 00:24:03 +0530

    build(config): add config-server

---
+++ config-server/src/test/java/com/waves/configserver/ConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.waves.configserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
