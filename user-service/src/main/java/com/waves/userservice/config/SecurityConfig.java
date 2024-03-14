0b1aa57bffed091b9a0980020e30863eca56ff0a
Samallll <samalchokli@gmail.com>
2024-03-14 11:11:14 +0530

    chore(config): add user-service configuration

--- user-service/src/main/java/com/waves/userservice/config/SecurityConfig.java
+++ user-service/src/main/java/com/waves/userservice/config/SecurityConfig.java
@@ -3,8 +3,6 @@ package com.waves.userservice.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.core.Ordered;
-import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.jwt.JwtDecoders;
